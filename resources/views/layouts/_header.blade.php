6786dc2f5f33098ac16d13b68d94270afb304969
2388426660 <2388426660@qq.com>
2019-02-28 10:39:44 +0800

    生成用户认证

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
-        <!-- Branding Image -->
         <a class="navbar-brand " href="{{ url('/') }}">
             MamerBBS
         </a>
@@ -9,16 +8,13 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
 
             </ul>
 
-            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
             </ul>
         </div>
     </div>
