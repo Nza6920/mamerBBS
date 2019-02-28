fc64649ac62de9677318f2ccc2e8edccad67826d
2388426660 <2388426660@qq.com>
2019-02-28 21:43:54 +0800

    user can register using phone

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\UserRequest;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function store(UserRequest $request, User $user)
+    {
+        $verifyData = \Cache::get($request->verification_key);
+
+        // key 不存在 or 失效
+        if (!$verifyData) {
+            return $this->response->error('验证码失效', 422);
+        }
+
+        // 验证码不正确
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            // 返回 401
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        // 创建用户
+        $user->name = $request->name;
+        $user->phone = $verifyData['phone'];
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        return $this->response->created();
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
     use Notifiable, MustVerifyEmailTrait;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'phone', 'password',
     ];
 
     protected $hidden = [
