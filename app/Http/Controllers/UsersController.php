2bbae73a737666708879f8dabb368e057ff0028a
2388426660 <2388426660@qq.com>
2019-03-08 00:26:59 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,12 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     // 用户个人界面
     public function show(User $user)
     {
@@ -17,12 +23,14 @@ class UsersController extends Controller
     // 编辑个人资料界面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     // 编辑个人资料
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
