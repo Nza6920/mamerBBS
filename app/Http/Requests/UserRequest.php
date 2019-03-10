cc10a03b6d31e9a9fe8a468af9b5638ec1bd9ee1
2388426660 <2388426660@qq.com>
2019-03-11 00:12:19 +0800

    第二次修复编辑个人资料的bug, 我真傻

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -21,7 +21,7 @@ class UserRequest extends FormRequest
         return [
             'name' => 'required|between:2,12|regex:/^[A-Za-z0-9\x{4e00}-\x{9fa5}]+$/u|unique:users,name,' . \Auth::id(),
             'introduction' => 'max:50',
-            'avatar' => 'mimetypes:image/jpeg,image/png,image/gif|dimensions:min_width=208,min_height=208|size:500'
+            'avatar' => 'mimetypes:image/jpeg,image/png,image/gif|dimensions:min_width=208,min_height=208|between:0,500'
         ];
     }
 
@@ -30,7 +30,7 @@ class UserRequest extends FormRequest
         return [
             'avatar.mimetypes' => '格式错啦, 必须是jpeg,png,jpg,gif格式的图片',
             'avatar.dimensions' => '清晰度不够哦，宽和高需要 208px 以上',
-            'avatar.size' => '太大啦, 图片要小于500kb',
+            'avatar.between' => '太大啦, 图片要小于500kb',
             'name.regex' => '格式错啦, 用户名只能由字母, 数字, 中文组成',
             'name.unique' => '重名啦, 用户名已被占用',
             'introduction.max' => '经历有点多哦, 个人简介最大不能超过50个字'
