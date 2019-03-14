67e60a821e28311e916c07f0068b3343b430dca3
2388426660 <2388426660@qq.com>
2019-03-14 22:12:55 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,23 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body',
+        'user_id', 'category_id',
+        'reply_count', 'view_count',
+        'last_reply_user_id', 'order',
+        'excerpt', 'slug'
+    ];
+
+    // 文章属于话题
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 文章属于用户
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
