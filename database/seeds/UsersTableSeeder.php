e4f37e3dccd33357781023e3dbe83a134a558083
2388426660 <2388426660@qq.com>
2019-03-15 10:41:59 +0800

    WYSIWYG 编辑器

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -41,7 +41,8 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'admin';
-        $user->email = 'qwerty';
+        $user->email = 'admin@mamer.com';
+        $user->password = bcrypt('qwerty');
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
 
