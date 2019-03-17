2647cb7ee90dc8b0c5815bf82b7cade992ef5d63
2388426660 <2388426660@qq.com>
2019-03-17 17:00:33 +0800

    修复新建话题中的链接标签bug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -13,13 +13,12 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+
         // xss 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
 
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
-
-
     }
 
     public function saved(Topic $topic)
