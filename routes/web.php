829cf4116acadce5f65d991bd2073bd2cfd7c637
2388426660 <2388426660@qq.com>
2019-03-31 12:59:52 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+// 无权限提醒路由
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
