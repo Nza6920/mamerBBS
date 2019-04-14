5684568605b0b2b116f36aaf416094a8ff5261a8
2388426660 <2388426660@qq.com>
2019-04-14 23:31:55 +0800

    notifications stats

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -7,10 +7,19 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
+    // 消息通知列表
     public function index()
     {
         $notifications = $this->user->notifications()->paginate(20);
 
         return $this->response->paginator($notifications, new NotificationTransformer());
+    }
+
+    // 未读消息通知统计
+    public function stats()
+    {
+        return $this->response->array([
+            'unread_count' => $this->user()->notification_count,
+        ]);
     }
 }
