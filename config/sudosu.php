12e99e4819d66f19ec0faae96641f6fc1006147a
Nza6920 <1484663282@qq.com>
2020-03-13 17:38:41 +0800

    sudosu config

--- config/sudosu.php
+++ config/sudosu.php
@@ -4,7 +4,7 @@ return [
     'enable' => env('SUDOSU_ENABLE', true),
 
     // 允许使用的顶级域名
-    'allowed_tlds' => ['dev', 'local', 'test'],
+    'allowed_tlds' => ['dev', 'local', 'test', 'club'],
 
     // 用户模型
     'user_model' => App\Models\User::class
